173b9b2a193e51bf9b4c246c09f1b7faac4f8aea
Riyadani <113497020+Riyadani@users.noreply.github.com>
2023-07-25 21:33:42 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home || TataStriveOfficial</title>
+  <link rel="shortcut icon" href="images/favicon.ico">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/nav.css">
+  <link rel="stylesheet" href="css/media.css">
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald|Open+Sans:400,400italic,600italic,600">
+  <?php require 'header.php'; ?>
+</head>
+
+
+<body style="background-color: #000;">
+  <?php require 'nav_bar.php'; ?>
+  <div style="background: #000;">
+    <center><img src="images/aboutimg blk.svg" style="width:50%;"></center>
+
+    <div id="content" style="background-color:#000;">
+      <h1> <u> WHAT WE ARE...</u></h1>
+      <p>An exclusive methodology to help youth make the correct career choice.A technology platform in sync with changing times that makes operations, republic, scalable and measurable.An excellent model for collaboration of group companies, non-profits, goverment agencies, foundations, truts and banks. A momentous feat in the skilling landscape s first-its-kind skilling programe in odisha itis.</p><br>
+      <p>Skill development training center is a platform to enhance the skill and make the students industry ready in training is crucial for organizational development and its success which is indeed fruitful to both employers and employees of an organization.Here are some benifits of training and development.</p><br>
+      <div class="row">
+        <div class="col-md-12 midd">
+          <img src="images/logfull1.png" id="imgg" style="width:100% ;height: 100%;">
+        </div>
+        <br style="line-height: 10px;">
+        <br>
+      </div>
+      <br>
+    </div>
+  </div>
+  <div class="row row-cols-1 row-cols-md-3" style="background: #000;">
+
+    <div class="col-mb-4">
+      <div class="card card_back ca" style="margin-left: 20px;">
+        <div class="card-body card_back">
+          <h2> <u> SAP</u></h2><br>
+          <p>SAP means Business Intelligence Tools and Solutions wiche is "Systems Applications and products in data processing".The SAP Software system was developed in 1971 by five IBM engineers, Hoop, Wellenreuther, Hector, Tschira and Plattner, who worked together on an internal project, In June 1972, they left IBM and founded the SAP.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-mb-4">
+      <div class="card card_back">
+        <div class="card-body card_back ca ">
+          <h2> <u>TATA STRIVE</u></h2><br>
+          <p>Tata Strive is the skill development initiative of the tata comunity initiative Trust under the agies of TATA trusts, addressing the pressing need of skilling india's youth for employemnt, entrepreneurship and community enterprise.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-mb-4">
+      <div class="card card_back ca ">
+        <div class="card-body card_back">
+          <h2> <u>GENERATION INDIA</u></h2><br>
+          <P>GENERATION INDIA our mission: we transform education to employement systems to prepare,place and support people into life-chsnging carrers that would otherwise be inaccessible.</P>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <div>
+    <!-- space for the image -->
+  </div>
+  <?php require 'footer.php'; ?>
+</body>
+
+</html>
